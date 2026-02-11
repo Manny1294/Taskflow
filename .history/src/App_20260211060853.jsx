dfa4673ae266b357ca83d9919bd35627d8bb5517
Emmanuel Odili <odiliosezua@gmail.com>
2026-02-11 09:04:44 +0100

    chore: ignore editor history

--- .history/src/App_20260211060853.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <h1>
-        <TaskFlow />
-      </h1>
-    </>
-  );
-}
--- .history/src/App_20260211063931.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <h1 className="text-red-400 bg-blue-200 flex items-center justify-center h-screen">
-        TaskFlow
-      </h1>
-    </>
-  );
-}
